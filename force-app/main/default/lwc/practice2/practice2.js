c444b86c3467949ccc35b7ffe7bb9f3f87d378cc
SrivaniAnantharaju <srivanianantharaju@gmail.com>
2024-09-03 14:19:47 -0400

    added lwc

---
+++ force-app/main/default/lwc/practice2/practice2.js
@@ -0,0 +1,6 @@
+import { LightningElement } from 'lwc';
+
+export default class Practice2 extends LightningElement {
+
+    Hello1
+}
---
+++ force-app/main/default/lwc/pratice3/pratice3.js
@@ -0,0 +1,6 @@
+import { LightningElement } from 'lwc';
+
+export default class Pratice3 extends LightningElement {
+
+    Hello3
+}
